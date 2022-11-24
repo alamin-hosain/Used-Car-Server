6f5cae4ad3313fe21bffbbcdd58b734fa3a17cea
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-11-24 20:17:18 +0600

    finish products

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv')
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middle ware
+dotenv.config()
 app.use(cors());
 app.use(express.json());
 
 
-// Server running adn Check Api
-app.get('/', (req, res) => {
-    res.send('Server up and Running')
-})
 
-app.get(port, () => {
-    console.log('Server on port: ', port);
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gcdspqh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+async function run() {
+    try {
+
+        const usedProductsCollection = client.db('UsedCar').collection('products');
+
+
+        // getting all the used products
+        app.get('/used-cars', async (req, res) => {
+            const usedcars = {};
+            const result = await usedProductsCollection.find(usedcars).toArray();
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(e => console.log(e))
+
+
+
+
+
+
+
+
+// Server running adn Check Api
+app.get('/', async (req, res) => { res.send('Used Car Server Running') })
+app.listen(port, () => console.log('Server running on port', port))
